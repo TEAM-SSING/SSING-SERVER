b5b98e39b175c82831b889ce9df0a84e9d1824db
yujeong <parkyj0430@gmail.com>
2026-06-25 13:06:17 +0900

    init: 프로젝트 초기 설정

---
+++ .github/ISSUE_TEMPLATE/feat.md
@@ -0,0 +1,14 @@
+---
+name: "✨ Feature Request"
+about: 새로운 기능을 개발할 때 사용합니다.
+title: "[Feat] "
+labels: "✨ FEAT"
+assignees: ''
+---
+
+## 📃 기능 설명
+<!-- 어떤 기능을 개발할 예정인지 작성해주세요. -->
+
+## ✅ To-do
+- [ ]
+- [ ]
---
+++ src/main/java/org/sopt/ssingserver/SsingServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.ssingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsingServerApplication.class, args);
+	}
+
+}
