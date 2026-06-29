af9258ebbfd227313405c21bf7b8cbbd721ed429
yujeong <parkyj0430@gmail.com>
2026-06-29 16:39:43 +0900

    chore: 프로필 기반 application 설정 및 .env 관리 추가

--- src/main/java/org/sopt/ssingserver/SsingServerApplication.java
+++ src/main/java/org/sopt/ssingserver/SsingServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SsingServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsingServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SsingServerApplication.class, args);
+    }
 
 }
