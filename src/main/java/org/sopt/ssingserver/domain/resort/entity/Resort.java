6d94317d05d1f754b410f8e4d97da056aab712a9
yujeong <parkyj0430@gmail.com>
2026-06-29 18:02:22 +0900

    feat: Resort에 식별 코드(code) 컬럼 추가

--- src/main/java/org/sopt/ssingserver/domain/resort/entity/Resort.java
+++ src/main/java/org/sopt/ssingserver/domain/resort/entity/Resort.java
@@ -21,6 +21,9 @@ public class Resort extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50, unique = true)
+    private String code;
+
     @Column(nullable = false, length = 100)
     private String name;
 }
