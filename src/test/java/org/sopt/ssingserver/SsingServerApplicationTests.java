92415e581f26bc4c0a94b439ef09cbd341179d88
yujeong <parkyj0430@gmail.com>
2026-07-05 13:10:35 +0900

    test: FCM 토큰 저장소 mock 빈 추가

--- src/test/java/org/sopt/ssingserver/SsingServerApplicationTests.java
+++ src/test/java/org/sopt/ssingserver/SsingServerApplicationTests.java
@@ -5,6 +5,7 @@ import org.sopt.ssingserver.domain.auth.repository.OAuthAccountRepository;
 import org.sopt.ssingserver.domain.auth.repository.RefreshTokenRepository;
 import org.sopt.ssingserver.domain.instructor.repository.InstructorProfileRepository;
 import org.sopt.ssingserver.domain.member.repository.MemberRepository;
+import org.sopt.ssingserver.domain.notification.repository.FcmTokenRepository;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
@@ -25,6 +26,9 @@ class SsingServerApplicationTests {
 
 	@MockitoBean
 	private InstructorProfileRepository instructorProfileRepository;
+
+	@MockitoBean
+	private FcmTokenRepository fcmTokenRepository;
 
 	@MockitoBean
 	private PlatformTransactionManager transactionManager;
