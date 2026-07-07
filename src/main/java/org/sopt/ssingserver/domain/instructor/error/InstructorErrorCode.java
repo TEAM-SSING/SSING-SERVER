35fd322062ff3f4b9192f83a3ebafad5831b0789
yujeong <parkyj0430@gmail.com>
2026-07-08 03:30:40 +0900

    refactor: 미사용 에러 코드 정리

--- src/main/java/org/sopt/ssingserver/domain/instructor/error/InstructorErrorCode.java
+++ src/main/java/org/sopt/ssingserver/domain/instructor/error/InstructorErrorCode.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum InstructorErrorCode implements ErrorCode {
 
-    INSTRUCTOR_NOT_APPROVED(HttpStatus.FORBIDDEN, "승인되지 않은 강사입니다."),
     ACTIVE_LESSON_EXISTS(HttpStatus.CONFLICT, "현재 진행 중인 강습이 있어 즉시노출을 시작할 수 없습니다."),
     INSTRUCTOR_RESORT_NOT_SET(HttpStatus.NOT_FOUND, "활동 리조트가 등록되지 않았습니다.");
 
