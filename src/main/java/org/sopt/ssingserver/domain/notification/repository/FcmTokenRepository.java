554ef31abeca4bdb6ddddc93e0c11602d235a342
yujeong <parkyj0430@gmail.com>
2026-07-16 05:03:30 +0900

    fix: FCM 무효 토큰 자동 삭제 처리

--- src/main/java/org/sopt/ssingserver/domain/notification/repository/FcmTokenRepository.java
+++ src/main/java/org/sopt/ssingserver/domain/notification/repository/FcmTokenRepository.java
@@ -1,12 +1,18 @@
 package org.sopt.ssingserver.domain.notification.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.sopt.ssingserver.domain.notification.entity.FcmToken;
+import org.sopt.ssingserver.domain.notification.enums.ClientApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 
     Optional<FcmToken> findByToken(String token);
 
+    List<FcmToken> findAllByMemberIdAndClientApp(Long memberId, ClientApp clientApp);
+
     void deleteByMemberIdAndToken(Long memberId, String token);
+
+    void deleteByToken(String token);
 }
